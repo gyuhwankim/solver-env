79dd2953f0ced178e924ee9355950998df9a3b8e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-17 20:28:29 +0900

    Improve usage of new command

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func createApplication() *cli.App {
 	app.Commands = []cli.Command{
 		{
 			Name:    "new",
-			Usage:   "Create new environment",
+			Usage:   "Create new environment ex) ps-cli new <options> <src>",
 			Action: func(context *cli.Context) error {
 				template := context.String("templates")
 				src := context.Args().Get(0)
