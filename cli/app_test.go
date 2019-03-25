7de316511089449d4479c9c4ea225e1877a3a9a8
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-03-25 14:10:33 +0000

    Add unittest of Run method when cli engine is nil

--- cli/app_test.go
+++ cli/app_test.go
@@ -1,1 +1,16 @@
 package cli
+
+import (
+	"testing"
+)
+
+func TestRunWhenCliNil(t *testing.T) {
+	app := App{cli: nil}
+	expected := ErrEmptyCliEngine
+	
+	actual := app.Run([]string{})
+
+	if expected != actual {
+		t.Errorf("expected: %t, but actual: %t", expected, actual)
+	}
+}
