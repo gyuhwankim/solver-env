eb0eac31ba8ab08ec0f34a461582e9782e143f69
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-03-25 13:26:49 +0000

    Organize app structure

--- cli/app.go
+++ cli/app.go
@@ -1,2 +1,32 @@
 package cli
 
+import (
+	"gopkg.in/urfave/cli.v1"
+)
+
+var appData = map[string]string{
+	"Name": "solverenv",
+	"Usage": "Provide an environment where can solve problems",
+}
+
+type Runner interface {
+	Run(args []string) error
+}
+
+type App struct {
+	cli *cli.App
+}
+
+func (app *App) Run(args []string) error {
+	return app.cli.Run(args)	
+}
+
+func NewApp() Runner {
+	cliEngine := cli.NewApp()
+	cliEngine.Name = appData["Name"]
+	cliEngine.Usage = appData["Usage"]
+	
+	app := App{cliEngine}
+	
+	return &app	
+}
