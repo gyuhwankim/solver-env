8b95a3290ea8b1b47d2c75ea949219cb1ae530c1
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-01 14:57:39 +0000

    Add convenient methods that get metadata

--- cli/app.go
+++ cli/app.go
@@ -5,18 +5,28 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+const (
+	DATA_NAME = "Name"
+	DATA_USAGE = "Usage"
+	DATA_VERSION = "Version"
+)
+
 // metadata of cli included items like name, usage and version.
 var appData = map[string]string{
-	"Name":    "solverenv",
-	"Usage":   "Provide an environment where can solve problems",
-	"Version": "v0.1.0",
+	DATA_NAME:    "solverenv",
+	DATA_USAGE:   "Provide an environment where can solve problems",
+	DATA_VERSION: "v0.1.0",
 }
 
 var appCommands = []Commander{
 	newInitCommand(),
 }
 
-var ErrEmptyCliEngine = errors.New("App not contain cli engine.")
+var (
+	ErrEmptyCliEngine = errors.New("App not contain cli engine.")
+	ErrNotContainItem = errors.New("Not contain item in collection.")
+)
+
 
 type Runner interface {
 	Run(args []string) error
@@ -24,6 +34,28 @@ type Runner interface {
 
 type App struct {
 	cli *cli.App
+	metaData map[string]string
+}
+
+func (app App) Name() (string, error) {
+	return app.getData(DATA_NAME)	
+}
+
+func (app App) Usage() (string, error) {
+	return app.getData(DATA_USAGE)
+}
+
+func (app App) Version() (string, error) {
+	return app.getData(DATA_VERSION)	
+}
+
+func (app App) getData(key string) (string, error) {
+	item, exists := app.metaData[key]
+	if !exists {
+		return "", ErrNotContainItem
+	}
+	
+	return item, nil
 }
 
 func (app *App) Run(args []string) error {
