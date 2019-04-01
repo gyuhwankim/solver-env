14edf3d85b8310dcb728a3adc3d1067f1d693552
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-01 13:28:25 +0000

    Add updateAppMetadata function with test

--- cli/app.go
+++ cli/app.go
@@ -5,15 +5,16 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+// metadata of cli included items like name, usage and version.
 var appData = map[string]string{
-	"Name": "solverenv",
-	"Usage": "Provide an environment where can solve problems",
+	"Name":    "solverenv",
+	"Usage":   "Provide an environment where can solve problems",
 	"Version": "v0.1.0",
 }
 
 var appCommands = []Commander{
 	newInitCommand(),
-} 
+}
 
 var ErrEmptyCliEngine = errors.New("App not contain cli engine.")
 
@@ -27,10 +28,10 @@ type App struct {
 
 func (app *App) Run(args []string) error {
 	if app.cli == nil {
-		return ErrEmptyCliEngine		
+		return ErrEmptyCliEngine
 	}
-	
-	return app.cli.Run(args)	
+
+	return app.cli.Run(args)
 }
 
 func NewApp() Runner {
@@ -43,8 +44,16 @@ func NewApp() Runner {
 	for _, command := range appCommands {
 		cliEngine.Commands = append(cliEngine.Commands, createCliCommand(command))
 	}
-	
+
 	app := App{cli: cliEngine}
-	
-	return &app	
+
+	return &app
+}
+
+func updateAppMetadata(cli *cli.App, appMetadata map[string]string) error {
+	cli.Name = appMetadata["Name"]
+	cli.Usage = appMetadata["Usage"]
+	cli.Version = appMetadata["Version"]
+
+	return nil
 }
--- cli/app_test.go
+++ cli/app_test.go
@@ -1,16 +1,35 @@
 package cli
 
 import (
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/urfave/cli.v1"
 	"testing"
 )
 
 func TestRunWhenCliNil(t *testing.T) {
 	app := App{cli: nil}
 	expected := ErrEmptyCliEngine
-	
+
 	actual := app.Run([]string{})
 
 	if expected != actual {
 		t.Errorf("expected: %t, but actual: %t", expected, actual)
 	}
+}
+
+func TestUpdateAppMetadata(t *testing.T) {
+	metaData := map[string]string{
+		"Name":    "test-solverenv",
+		"Usage":   "just test solverenv",
+		"Version": "test version",
+	}
+
+	cliEngine := cli.NewApp()
+	if err := updateAppMetadata(cliEngine, metaData); err != nil {
+		t.Errorf("Occurred error: %s", err)
+	}
+
+	assert.Equal(t, cliEngine.Name, metaData["Name"])
+	assert.Equal(t, cliEngine.Usage, metaData["Usage"])
+	assert.Equal(t, cliEngine.Version, metaData["Version"])
 }
